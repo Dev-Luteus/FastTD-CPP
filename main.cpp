cf3ad14bcd7db5b43104b70b86728d197efd11c6
Dev-Luteus <EmilDev.Luteus@gmail.com>
2025-03-12 19:16:14 +0100

    Git Init C++ TD Project

---
+++ main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <raylib.h>
+
+int main()
+{
+    constexpr int screenWidth = { 1920 };
+    constexpr int screenHeight = { 1080 };
+
+    SetTargetFPS(200);
+    InitWindow(screenHeight, screenWidth, "FastTD");
+
+    while (!WindowShouldClose())
+    {
+
+    }
+
+    return 0;
+}
