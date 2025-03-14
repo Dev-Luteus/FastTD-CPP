ae6bc861aca204dd7c8afcd5116376069de90241
Dev-Luteus <EmilDev.Luteus@gmail.com>
2025-03-14 18:14:38 +0100

    Refactored cell into struct, removed cell.cpp, added Grid.cpp, grid h.

---
+++ grid.cpp
@@ -0,0 +1,40 @@
+﻿#include "grid.h"
+#include "cell.h"
+#include <stdexcept>
+
+void Grid::GenerateGrid()
+{
+    for (int y = 0; y < GRID_HEIGHT; ++y)
+    {
+        for (int x = 0; x < GRID_WIDTH; ++x)
+        {
+            cells[x][y].id = 0;
+        }
+    }
+}
+
+void Grid::DrawGrid() // in the works
+{
+
+}
+
+
+void Grid::CheckBounds(int x, int y) const
+{
+    if (x < 0 || x >= GRID_WIDTH || y < 0 || y >= GRID_HEIGHT)
+    {
+        throw std::out_of_range("Cell coordinates out of range");
+    }
+}
+
+Cell &Grid::ModifyCell(int x, int y)
+{
+    CheckBounds(x, y);
+    return cells[x][y];
+}
+
+const Cell& Grid::GetCell(int x, int y) const
+{
+    CheckBounds(x, y);
+    return cells[x][y];
+}
---
+++ grid.h
@@ -0,0 +1,29 @@
+﻿#ifndef GRID_H
+#define GRID_H
+
+#include <array>
+#include "cell.h"
+
+class Grid
+{
+    private:
+    static constexpr int GRID_WIDTH { 40 };
+    static constexpr int GRID_HEIGHT { 40 };
+
+    // Grid width containing Cell rows by height
+    std::array<std::array<Cell, GRID_HEIGHT>, GRID_WIDTH> cells;
+
+    void CheckBounds(int x, int y) const;
+
+    public:
+    void GenerateGrid();
+    void DrawGrid();
+
+    [[nodiscard]] Cell& ModifyCell(int x, int y);
+    [[nodiscard]] const Cell& GetCell(int x, int y) const;
+
+    [[nodiscard]] static constexpr int GetWidth() { return GRID_WIDTH; }
+    [[nodiscard]] static constexpr int GetHeight() { return GRID_HEIGHT; }
+};
+
+#endif //GRID_H
