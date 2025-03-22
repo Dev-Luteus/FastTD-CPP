443f080200ae521436964bcef0888e47f0334910
Dev-Luteus <EmilDev.Luteus@gmail.com>
2025-03-22 17:57:46 +0100

    Update enemy.h

--- include/enemy.h
+++ include/enemy.h
@@ -9,7 +9,7 @@
 class Enemy
 {
 private:
-    static constexpr float ENEMY_MOVE_SPEED { 0.75f }; // X cell per second ( lower value = faster )
+    static constexpr float ENEMY_MOVE_SPEED { 0.5f }; // X cell per second ( lower value = faster )
     static constexpr int ENEMY_DAMAGE { 5 };
 
     Texture2D texture { 0 };
